53adfdf31ed5d9986b26d431d916d9f97b0486c8
accicttech1 <dannyfalero@acc.edu.au>
2020-12-09 09:02:00 +1100

    Re-added links to proper places but are ignored

--- src/pages/index.js
+++ src/pages/index.js
@@ -266,16 +266,16 @@ class RootIndex extends React.Component  {
                     <br />
                     {/*<LocationList />*/}
                     <ul>
-                        <li><Link to={"#"} id={"Moreton (QLD)"} onClick={setFilterItem}>Moreton ({Mor})</Link></li>
-                        <li><Link to={"#"} id={"Singleton (NSW)"} onClick={setFilterItem}>Singleton ({Sing})</Link></li>
-                        <li><Link to={"#"} id={"Brightwaters (NSW)"} onClick={setFilterItem}>Brightwaters ({Bright})</Link></li>
-                        <li><Link to={"#"} id={"Marsden Park (NSW)"} onClick={setFilterItem}>Marsden Park ({Mp})</Link></li>
-                        <li><Link to={"#"} id={"Hume (VIC)"} onClick={setFilterItem}>Hume ({Hume})</Link></li>
-                        <li><Link to={"#"} id={"Darling Downs (WA)"} onClick={setFilterItem}>Darling Downs ({Dar})</Link></li>
-                        <li><Link to={"#"} id={"Southlands (SOU)"} onClick={setFilterItem}>Southlands ({Sou})</Link></li>
-                        <li><Link to={"#"} id={"Burnie (TAS)"} onClick={setFilterItem}>Burnie ({Bur})</Link></li>
-                        <li><Link to={"#"} id={"Hobart (TAS)"} onClick={setFilterItem}>Hobart ({Hob})</Link></li>
-                        <li><Link to={"#"} id={"Launceston (TAS)"} onClick={setFilterItem}>Launceston ({Lau})</Link></li>
+                        <li><Link to="/moreton-qld" id={"Moreton (QLD)"} onClick={setFilterItem}>Moreton ({Mor})</Link></li>
+                        <li><Link to="/singleton-nsw" id={"Singleton (NSW)"} onClick={setFilterItem}>Singleton ({Sing})</Link></li>
+                        <li><Link to="/brightwaters-nsw" id={"Brightwaters (NSW)"} onClick={setFilterItem}>Brightwaters ({Bright})</Link></li>
+                        <li><Link to="/marsden-park-nsw" id={"Marsden Park (NSW)"} onClick={setFilterItem}>Marsden Park ({Mp})</Link></li>
+                        <li><Link to="/hume-vic" id={"Hume (VIC)"} onClick={setFilterItem}>Hume ({Hume})</Link></li>
+                        <li><Link to="/darling-downs-wa" id={"Darling Downs (WA)"} onClick={setFilterItem}>Darling Downs ({Dar})</Link></li>
+                        <li><Link to="/southland-wa" id={"Southlands (SOU)"} onClick={setFilterItem}>Southlands ({Sou})</Link></li>
+                        <li><Link to="/burnie" id={"Burnie (TAS)"} onClick={setFilterItem}>Burnie ({Bur})</Link></li>
+                        <li><Link to="/hobart-tas" id={"Hobart (TAS)"} onClick={setFilterItem}>Hobart ({Hob})</Link></li>
+                        <li><Link to="/launceston-tas" id={"Launceston (TAS)"} onClick={setFilterItem}>Launceston ({Lau})</Link></li>
                     </ul>
                 </FilterColumn>
                 <JobsList>
