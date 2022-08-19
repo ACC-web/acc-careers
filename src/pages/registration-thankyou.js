fa62a41752d3f34e091ea8c1eb2bbeefde3c5792
Christian Bathie <christian_bathie@bitbucket.org>
2022-08-19 16:09:52 +1000

    added contentful filter to careers

--- src/pages/registration-thankyou.js
+++ src/pages/registration-thankyou.js
@@ -1,18 +1,11 @@
 import React from 'react'
-import {graphql, Link} from 'gatsby'
+import {graphql} from 'gatsby'
 import get from 'lodash/get'
 import { Helmet } from 'react-helmet'
 import Hero from '../components/hero'
 import Layout from '../components/layout'
-import ArticlePreview from '../components/article-preview'
 import styled from "styled-components"
-import VideoGallery from '../components/video-gallery'
-import { StaticImage } from "gatsby-plugin-image"
 
-//Select Imports
-//https://react-select.com/home
-import Select from 'react-select';
-import makeAnimated from 'react-select/animated';
 
 const Top = styled.div`
   position:relative;
@@ -75,122 +68,13 @@ const Wrapper = styled.section`
   }
 `
 
-const FlexWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  
-  .text{
-    display: flex;
-    flex-direction: column;
-    order: 1;
-  }
-
-  @media(min-width: 768px){
-    display: block;
-  }
-`
-
-const Linkedin = styled.a`
-  width: 100%;
-  float: none;
-  border-left: 0;
-  padding: 0;
-  margin-top: 0;
-  margin-left: 0;
-  margin-bottom: 1rem;
-  display: flex;
-
-  order: 2;
-  
-  .logo{
-    width: 75%;
-    flex: 1;
-    margin: auto;
-  }
-  p{
-    flex: 2;
-    text-align: left;
-  }
-  
-  &:hover {
-    text-decoration: none;
-  }
-
-  @media(min-width: 768px){
-    display: block;
-    width: 200px;
-
-    float: right;
-    border-top: 0;
-    border-left: 1px solid #97A4B2;
-    padding: 10px 0 10px 30px;
-    margin-top: -30px;
-    margin-left: 30px;
-
-    .logo {
-      width: 100%;
-      margin: auto;
-    }
-
-    p {
-      text-align: left;
-      font-size: 100%;
-    }
-  }
-`
-
-
-const JobsContainer = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  
-  @media(min-width: 768px){
-    flex-direction: row
-  }
-`
-const FilterColumn = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  padding-right: 0;
-  @media(min-width: 768px){
-    width: 33%;
-    padding-right: 2rem;
-    border-right: 1px solid #97A4B2;
-  }
-`
-const JobsList = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  padding-left: 0;
-  
-  @media(min-width: 768px){
-    width: 75%;
-    padding-left: 2rem;
-  }
-  
-  .article-list{
-    padding: 0;
-    list-style-type: none;
-  }
-  li{
-    border-top: 1px solid #646E78;
-    
-    :last-child{
-      border-bottom: 1px solid #646E78;
-    }
-  }
-`
 
 class RegistrationThankyouPage extends React.Component  {
 // export default function AnimatedMulti() {
 
   render( data ) {
     const siteTitle = get(this, 'props.data.site.siteMetadata.title')
-    const siteUrl = get(this, 'props.data.site.siteMetadata.siteUrl')
-
+    //const siteUrl = get(this, 'props.data.site.siteMetadata.siteUrl')
 
     return (
       <Layout location={this.props.location}>
@@ -225,7 +109,7 @@ export const pageQuery = graphql`
     query ConfirmationQuery {
         site {
             siteMetadata {
-                title,
+                title
                 siteUrl
             }
         }
